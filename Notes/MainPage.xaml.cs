edffb0458c30224e866508870b9b0e6d3151624d
Diogo-poke <diogopda2007@gmail.com>
2025-08-25 21:55:16 -0300

    Alteração o metodo de salvar e Apagar

--- Notes/MainPage.xaml.cs
+++ Notes/MainPage.xaml.cs
@@ -1,27 +1,47 @@
 ﻿namespace Notes;
-
+   
     public partial class MainPage : ContentPage
     {
     int count = 0;
     string caminho = Path.Combine(FileSystem.AppDataDirectory,"arquivo");
-
-        public MainPage()
+    public MainPage()
         {
 
           InitializeComponent();
+        if(File.Exists(caminho))
+        CaixaEditor.Text = File.ReadAllText(caminho);
+        
 
-        }
 
-        private void SalvarBtn_Clicked(object sender, EventArgs e)
-        {
+    }
+
+    private void SalvarBtn_Clicked(object sender, EventArgs e)
+    {
         string conteudo = CaixaEditor.Text;
         File.WriteAllText(caminho, conteudo);
-        }
+
+
+        Console.WriteLine($"Arquivo salvo: {CaixaEditor}");
+        Console.WriteLine($"Localização: {caminho}");
+    }
 
         private void ApagarBtn_Clicked(object sender, EventArgs e)
         {
+          File.Delete(caminho);
+        DisplayAlert("Arquivo apagado", "Arquivo apagado com sucesso", "OK");
+
+         if(File.Exists(caminho))
+        {
+            File.Delete(caminho);
+            CaixaEditor.Text = null;
+            DisplayAlert("Arquivo apagado", "Arquivo apagado com sucesso", "OK");
+        }
+         else
+        {
+            DisplayAlert("Erro", "o arquivo não existe", "ok");
+        }
 
         }
     }
-
+ 
 
